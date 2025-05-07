99da042edd32f013ca91970f48a04da4bf0610b2
sagar-lawande <sagarlawande07@gmail.com>
2025-05-07 15:33:10 +0530

    message sent

---
+++ thanks.php
@@ -0,0 +1,16 @@
+<?php
+ $name = $_REQUEST['txtName'];
+ $email = $_REQUEST['txtEmail'];
+ $con = mysqli_connect("localhost", "root","", "sagar") or die("Error Connection Database");
+ $q=mysqli_query($con, "insert into
+ wad(name,email) values
+ ('$name','$email')");
+ if($q)
+ {
+ echo "Record inserted successfully";
+ }
+ else
+ {
+ echo mysqli_error($con);
+ }
+ ?>
